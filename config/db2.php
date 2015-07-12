e34469fc079046b3e03dfd37b449b59f59cd0c23
mofeiwo <mofeiwozl@163.com>
2015-07-12 09:16:15 +0800

    Update db2.php

--- config/db2.php
+++ config/db2.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.1.108;dbname=yii2advanced',
+    'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
